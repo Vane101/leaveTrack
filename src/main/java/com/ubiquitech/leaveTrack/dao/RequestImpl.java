5dd8f893040e319dfce55c1c7a8da0c7dc313349
Vane Zulu <vane@ubiquitech.co.za>
2015-01-21 09:15:52 +0200

    Removed straight SQL queries, added code to search for requests based on a few criteria.
    Added a FullCalendar implementation for all approved requests to be viewed graphically on a calendar.
    
    Created a Request Query Flow
    
    Removed supervisorID column from the request database as this information can be found under the employee table.
    
    A few Css additions

--- src/main/java/com/ubiquitech/leaveTrack/dao/RequestImpl.java
+++ src/main/java/com/ubiquitech/leaveTrack/dao/RequestImpl.java
@@ -1,9 +1,11 @@
 package com.ubiquitech.leaveTrack.dao;
 
 import com.ubiquitech.leaveTrack.domain.Request;
-import org.hibernate.Query;
+import com.ubiquitech.leaveTrack.form.RequestQueryForm;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,25 +27,84 @@ public class RequestImpl implements RequestDao {
     public void createRequest(Request request) {
         Session session = this.sessionFactory.getCurrentSession();
         session.saveOrUpdate(request);
-
     }
 
     @Override
     public List getRequestsByStatusAndSupervisorId(String state, Long id) {
         Session session = sessionFactory.openSession();
-        Query query = session.createQuery("FROM Request where supervisorId=:id and state=:state");
-        query.setLong("id",id);
-        query.setString("state",state);
-        return query.list();
+        Criteria request = session.createCriteria(Request.class);
+        Criteria employee = request.createCriteria("employee");
+        Criteria supervisor=employee.createCriteria("supervisor");
+        supervisor.add(Restrictions.eq("id",id));
+        request.add((Restrictions.eq("state",state)));
+        return request.list();
     }
 
     @Override
     public List getRequestsByStatusAndRequestId(String state, Long id) {
         Session session = sessionFactory.openSession();
-        Query query = session.createQuery("FROM Request where id= :id and state=:state");
-        query.setLong("id", id);
-        query.setString("state",state);
-       return  query.list();
+        Criteria request = session.createCriteria(Request.class);
+        request.add(Restrictions.eq("id",id));
+
+        if(!state.equals("")){
+            request.add(Restrictions.and(Restrictions.eq("state",state)));
+        }
+        return request.list();
+    }
+
+    @Override
+    public List getRequestsByState(String state) {
+        Session session = sessionFactory.openSession();
+        Criteria request = session.createCriteria(Request.class);
+        request.add(Restrictions.eq("state",state));
+        return request.list();
+    }
+
+    @Override
+    public List getQueriedRequests(RequestQueryForm requestQueryForm) {
+        String state=requestQueryForm.getState();
+        String leaveType=requestQueryForm.getLeaveType();
+        String employeeFirstName=requestQueryForm.getEmployeeFirstName();
+        String employeeLastName=requestQueryForm.getEmployeeLastName();
+        String supervisorFirstName=requestQueryForm.getSupervisorFirstName();
+        String supervisorLastName=requestQueryForm.getEmployeeLastName();
+        Long  requestId=requestQueryForm.getRequestId();
+
+        Session session = sessionFactory.openSession();
+
+        Criteria request = session.createCriteria(Request.class);
+        Criteria employee=request.createCriteria("employee");
+        Criteria supervisor=employee.createCriteria("supervisor");
+
+        if(!state.equals("")){
+            request.add(Restrictions.and(Restrictions.eq("state",state)));
+        }
+
+        if(!leaveType.equals("")){
+            request.add(Restrictions.and(Restrictions.eq("leaveType",leaveType)));
+        }
+
+        if(!(requestId==null)){
+            request.add(Restrictions.and(Restrictions.eq("id",requestId)));
+        }
+
+        if(!employeeFirstName.equals("")){
+            employee.add(Restrictions.and(Restrictions.eq("firstName",employeeFirstName)));
+        }
+
+        if(!employeeLastName.equals("")){
+            employee.add(Restrictions.and(Restrictions.eq("lastName",employeeLastName)));
+        }
+
+        if(!supervisorFirstName.equals("")){
+            supervisor.add(Restrictions.and(Restrictions.eq("firstName",supervisorFirstName)));
+        }
+
+        if(!supervisorLastName.equals("")){
+            supervisor.add(Restrictions.and(Restrictions.eq("lastName",supervisorLastName)));
+        }
+
+        return request.list();
     }
 
 }
